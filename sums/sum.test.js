39d3ec898e9e59241abee8203632b3af2d4cb6dd
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-04-19 11:00:42 +0300

    modified sum.test.js data

--- sums/sum.test.js
+++ sums/sum.test.js
@@ -1,21 +1,18 @@
 import { sum1, sum2, sum3, sum4 } from "./sum";
 
+const data = [1, 2, 3, 4, 5];
 it("check the sum1 with [1,2,3,4,5]", () => {
-  const data = [1, 2, 3, 4, 5];
   expect(sum1(data)).toBe(15);
 });
 
 it("check the sum2 with [1,2,3,4,5]", () => {
-  const data = [1, 2, 3, 4, 5];
   expect(sum2(data)).toBe(15);
 });
 
 it("check the sum3 with [1,2,3,4,5]", () => {
-  const data = [1, 2, 3, 4, 5];
   expect(sum3(data)).toBe(15);
 });
 
 it("check the sum4 with [1,2,3,4,5]", () => {
-  const data = [1, 2, 3, 4, 5];
   expect(sum4(data)).toBe(15);
 });
