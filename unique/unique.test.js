fbf8196f277ebac3eed64526e73df727b3427565
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-04-19 23:03:04 +0300

    fixed tests

--- unique/unique.test.js
+++ unique/unique.test.js
@@ -1,14 +1,14 @@
 import { unique1, unique2, unique3 } from "./unique";
 
-it("function return unique1 values from array", () => {
+it("function unique1 returns unique values from array", () => {
   const data = [1, 2, 2, 2, 3, 3, 1];
   expect(unique1(data)).toEqual([1, 2, 3]);
 });
-it("function return unique2 values from array", () => {
+it("function unique2 returns unique values from array", () => {
   const data = [1, 2, 2, 2, 3, 3, 1];
   expect(unique2(data)).toEqual([1, 2, 3]);
 });
-it("function return unique3 values from array", () => {
+it("function unique3 returns unique values from array", () => {
   const data = [1, 2, 2, 2, 3, 3, 1];
   expect(unique3(data)).toEqual([1, 2, 3]);
 });
